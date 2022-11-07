7962109852cfecfe82c0dac7930771c310ffc859
julian36alvarez <julian36alvarez@gmail.com>
2022-11-07 02:19:02 -0500

    CDK Aygo T-3

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ It is a [Maven](https://maven.apache.org/) based project, so you can open this p
 
 * [Java 8](https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html)
 * [Maven 3.6.0](https://maven.apache.org/download.cgi)
-* [Node.js 10.3.0](https://nodejs.org/en/download/)
+* [Node.js 16.15.1](https://nodejs.org/en/download/)
 * [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html)
 * [AWS CDK](https://docs.aws.amazon.com/cdk/latest/guide/getting_started.html)
 
@@ -23,4 +23,60 @@ It is a [Maven](https://maven.apache.org/) based project, so you can open this p
  * `cdk diff`        compare deployed stack with current state
  * `cdk docs`        open CDK documentation
 
-Enjoy!
+## Useful links
+
+ * [AWS CDK](https://docs.aws.amazon.com/cdk/latest/guide/home.html)
+ * [AWS CDK API Reference](https://docs.aws.amazon.com/cdk/api/latest/docs/aws-construct-library.html)
+
+## Resources
+
+  Git repository: https://github.com/julian36alvarez/React-Front-Aygo-Taller-3
+
+## Result
+  Result:
+
+** **
+**Emits the synthesized CloudFormation template**
+
+![img.png](img.png)
+** **
+**Deploy this stack to your default AWS account/region**
+
+![img_1.png](img_1.png)
+** **
+**Compare deployed stack with current state**
+
+![img_2.png](img_2.png)
+** **
+
+**Check instances**
+
+![img_5.png](img_5.png)
+
+** **
+**Instance 1**
+
+![img_7.png](img_7.png)
+
+** **
+
+**Instance 2 - 3**
+
+![img_8.png](img_8.png)
+
+****
+**Destroy instances**
+
+![img_4.png](img_4.png)
+
+
+
+## License
+
+This library is licensed under the MIT-0 License. See the LICENSE file.
+
+## Author
+
+*Julian Alvarez*
+
+
--- src/main/java/com/myorg/CdkAygoStack.java
+++ src/main/java/com/myorg/CdkAygoStack.java
@@ -7,6 +7,10 @@ import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.sns.Topic;
 import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
 import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.ec2.*;
+
+
+
 
 public class CdkAygoStack extends Stack {
     public CdkAygoStack(final Construct parent, final String id) {
@@ -21,9 +25,69 @@ public class CdkAygoStack extends Stack {
                 .build();
 
         final Topic topic = Topic.Builder.create(this, "CdkAygoTopic")
-            .displayName("My First Topic Yeah")
-            .build();
+                .displayName("My First Topic Yeah")
+                .build();
 
         topic.addSubscription(new SqsSubscription(queue));
+
+        //Define EC2 instance type t2.micro free tier
+        InstanceType instanceType = InstanceType.of(InstanceClass.T2, InstanceSize.MICRO);
+
+        //Machine image (AMI) for the instance
+        IMachineImage machineImage = MachineImage.latestAmazonLinux();
+
+        Vpc vpc = Vpc.Builder.create(this, "VPC")
+                .maxAzs(2)
+                .enableDnsHostnames(true)
+                .enableDnsSupport(true)
+                .build();
+
+        //Create a security group
+        SecurityGroup securityGroup = SecurityGroup.Builder.create(this, "SecurityGroup")
+                .vpc(vpc)
+                .allowAllOutbound(true)
+                .build();
+
+        //vpc subnet public
+        SubnetSelection subnetSelection = SubnetSelection.builder()
+                .subnetType(SubnetType.PUBLIC)
+                .build();
+
+        //security group for instance ports
+        securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(22), "allow SSH access from the world");
+        securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(80), "allow HTTP access from the world");
+        securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3000), "allow react port access from the world");
+        securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3001), "allow react port access from the world");
+
+        //load user data script
+        UserData userDataSCript = UserData.forLinux();
+        userDataSCript.addCommands("sudo su");
+        userDataSCript.addCommands("yum update -y");
+        userDataSCript.addCommands("yum install -y git");
+        userDataSCript.addCommands("curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -");
+        userDataSCript.addCommands("yum install -y nodejs");
+        userDataSCript.addCommands("yum install -y npm");
+        userDataSCript.addCommands("cd /home/ec2-user");
+        userDataSCript.addCommands("git clone https://github.com/julian36alvarez/React-Front-Aygo-Taller-3.git");
+        userDataSCript.addCommands("cd React-Front-Aygo-Taller-3");
+        userDataSCript.addCommands("npm install");
+        userDataSCript.addCommands("npm start");
+
+        //Create 3 instances with public dns and load react app on each instance
+        for (int i = 0; i < 3; i++) {
+            Instance.Builder.create(this, "Instance-" + i)
+                    .instanceType(instanceType)
+                    .machineImage(machineImage)
+                    .vpc(vpc)
+                    .vpcSubnets(subnetSelection)
+                    .allowAllOutbound(true)
+                    .securityGroup(securityGroup)
+                    .userData(userDataSCript)
+                    //you can create your own key pair and use it here
+                    .keyName("aygo-key")
+                    .build();
+        }
+
+
     }
 }
